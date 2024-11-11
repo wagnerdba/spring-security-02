ad494a3d570027be72f11005e92aeb210530018f
Wagner Pires <wagnerdba@gmail.com>
2024-11-11 18:17:24 -0300

    Lombok bug

---
+++ client-auth-application/src/test/java/com/wrtecnologia/clientauthapplication/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrtecnologia.clientapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
