269d424d14b8dd0e8460187d9b45db00857b9b9c
Wagner Pires <wagnerdba@gmail.com>
2024-11-11 19:42:40 -0300

    Tests added

---
+++ client-auth-application/src/test/java/com/wrtecnologia/clientauthapplication/ClientAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrtecnologia.clientauthapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ client-noauth-application/src/test/java/com/wrtecnologia/clientnoauthapplication/ClientNoAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrtecnologia.clientnoauthapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientNoAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
